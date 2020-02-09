f81d43bf13afbed244ca410ab1364ffbeab2a363
Gregor Martynus <gregor@martynus.net>
2020-02-09 14:49:21 -0800

    build(update-endpoints): generate separate `docs/*/*.md` files for each endpoint

--- scripts/update-endpoints/docs.js
+++ scripts/update-endpoints/docs.js
@@ -1,50 +1,82 @@
-const { readFileSync, writeFileSync } = require("fs");
-const { join } = require("path");
-
+const { outputFileSync } = require("fs-extra");
 const prettier = require("prettier");
 
 const ENDPOINTS = require("./generated/endpoints.json");
 
-const README_PATH = join(__dirname, "..", "..", "README.md");
-
-const newRoutes = {};
-
 generateRoutes();
 
 async function generateRoutes() {
-  const examples = ENDPOINTS.map(endpoint => {
+  const endpoints = ENDPOINTS.filter(endpoint => {
     if (endpoint.isLegacy && !/^\/teams\/\{team_id\}/.test(endpoint.url)) {
       // ignore legacy endpoints with the exception of the new teams legacy methods
-      return;
+      return false;
     }
 
-    const paramNames = endpoint.parameters
-      .filter(parameter => !parameter.alias)
-      .filter(parameter => !parameter.name.includes("."))
-      .filter(parameter => !["per_page", "page"].includes(parameter.name))
-      .map(parameter => parameter.name)
-      .join(", ");
+    return true;
+  });
 
-    const comment = endpoint.renamed
-      ? `// DEPRECATED: octokit.${endpoint.renamed.before.scope}.${endpoint.renamed.before.id}() has been renamed to octokit.${endpoint.renamed.after.scope}.${endpoint.renamed.after.id}()`
-      : `// ${endpoint.documentationUrl}`;
+  for (const endpoint of endpoints) {
+    const path = `docs/${endpoint.scope}/${endpoint.id}.md`;
+    outputFileSync(path, template(endpoint));
+    console.log(`${path} written`);
+  }
+}
 
-    return `${comment}
-octokit.${endpoint.scope}.${endpoint.id}(${
-      paramNames ? `{ ${paramNames} }` : ""
-    });`;
-  }).join("\n\n");
+function template(endpoint) {
+  const deprecationNotice = endpoint.isDeprecated
+    ? "**This method is deprecated.**"
+    : "";
+  const renameNotice = endpoint.renamed
+    ? `**Deprecated:** This method has been renamed to ${endpoint.renamed.after.scope}.${endpoint.renamed.after.id}`
+    : "";
 
-  const currentContent = readFileSync(README_PATH, "utf8");
-  const newContent = currentContent.replace(
-    /const octokit = new MyOctokit\({ auth: "secret123" }\);[\s\S]*\`/,
-    `const octokit = new MyOctokit({ auth: "secret123" });
+  const parameterRows = endpoint.parameters.map(
+    param =>
+      `<tr><td>${param.name}</td><td>${param.required ? "yes" : "no"}</td><td>
 
-${prettier.format(examples, {
-  parser: "babel"
-})}\`\`\``
+${param.description}
+
+</td></tr>`
   );
 
-  writeFileSync(README_PATH, newContent);
-  console.log(`${README_PATH} updated.`);
+  const requiredParameterNames = endpoint.parameters
+    .filter(parameter => parameter.required)
+    .map(parameter => parameter.name);
+  const example = `octokit.${endpoint.scope}.${
+    endpoint.id
+  }(${requiredParameterNames.join(", ")})`;
+
+  const content = `
+# ${endpoint.name}
+
+${deprecationNotice}
+
+${renameNotice}
+
+${endpoint.description}
+
+\`\`\`js
+${example}
+\`\`\`
+
+## Parameters
+
+<table>
+  <thead>
+    <tr>
+      <th>name</th>
+      <th>required</th>
+      <th>description</th>
+    </tr>
+  </thead>
+  <tbody>
+    ${parameterRows.join("\n")}
+  </tbody>
+</table>
+
+See also: [GitHub Developer Guide documentation](endpoint.documentationUrl).`;
+
+  return prettier.format(content, {
+    parser: "markdown"
+  });
 }
