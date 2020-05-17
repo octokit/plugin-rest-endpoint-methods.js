b3811a10c39512b05f3271b4332da4814e003d9e
Gregor Martynus <gregor@martynus.net>
2020-05-17 15:01:55 -0700

    fix: `git.gists.update({ gist_id, files }) (#84)

---
+++ test/issues.test.ts
@@ -0,0 +1,41 @@
+import fetchMock from "fetch-mock";
+import { Octokit } from "@octokit/core";
+
+import { restEndpointMethods } from "../src";
+
+describe("https://github.com/octokit/plugin-rest-endpoint-methods.js/issues/83", () => {
+  it("git.gists.update({ gist_id, files })", async () => {
+    const mock = fetchMock.sandbox().patchOnce(
+      "path:/gists/gist123",
+      { ok: true },
+      {
+        body: {
+          files: {
+            "postcss.config.js": {
+              content: "update from api",
+              filename: "new-postcss.config.js",
+            },
+          },
+        },
+      }
+    );
+
+    const MyOctokit = Octokit.plugin(restEndpointMethods);
+    const octokit = new MyOctokit({
+      auth: "secret123",
+      request: {
+        fetch: mock,
+      },
+    });
+
+    await octokit.gists.update({
+      gist_id: "gist123",
+      files: {
+        "postcss.config.js": {
+          content: "update from api",
+          filename: "new-postcss.config.js",
+        },
+      },
+    });
+  });
+});
