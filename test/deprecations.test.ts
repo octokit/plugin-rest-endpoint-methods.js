2ed85bec699b0d31fc20af18f9d5163dfd5c21dd
Gregor Martynus <gregor@martynus.net>
2020-05-18 10:16:18 -0700

    test: deprecated parameter

--- test/deprecations.test.ts
+++ test/deprecations.test.ts
@@ -52,72 +52,107 @@ describe("Deprecations", () => {
     expect(warnCalledCount).toEqual(1);
   });
 
-  // it("deprecated parameter", async () => {
-  //   const mock = fetchMock
-  //     .sandbox()
-  //     .getOnce("path:/repos/octocat/hello-world/commits/sha123", {
-  //       ok: true
-  //     });
-  //   const MyOctokit = Octokit.plugin(restEndpointMethods);
-  //   let warnCalledCount = 0;
-  //   const octokit = new MyOctokit({
-  //     request: {
-  //       fetch: mock
-  //     },
-  //     log: {
-  //       warn: (deprecation: Error) => {
-  //         warnCalledCount++;
-  //         expect(deprecation).toMatch(
-  //           '"commit_sha" parameter is deprecated for "octokit.repos.getCommit()". Use "ref" instead'
-  //         );
-  //       }
-  //     }
-  //   });
-  //   // See https://developer.github.com/v3/issues/comments/#create-a-comment
-  //   const { data } = await octokit.repos.getCommit({
-  //     owner: "octocat",
-  //     repo: "hello-world",
-  //     commit_sha: "sha123"
-  //   });
-  //   expect(data).toStrictEqual({ ok: true });
-  //   expect(warnCalledCount).toEqual(1);
-  // });
-  // it("deprecated parameter + new parameter", async () => {
-  //   const mock = fetchMock.sandbox().post(
-  //     "path:/repos/octocat/hello-world/issues/456/comments",
-  //     {
-  //       ok: true
-  //     },
-  //     {
-  //       body: {
-  //         body: "Hello there!"
-  //       }
-  //     }
-  //   );
-  //   const MyOctokit = Octokit.plugin(restEndpointMethods);
-  //   let warnCalledCount = 0;
-  //   const octokit = new MyOctokit({
-  //     request: {
-  //       fetch: mock
-  //     },
-  //     log: {
-  //       warn: (deprecation: Error) => {
-  //         warnCalledCount++;
-  //         expect(deprecation).toMatch(
-  //           '"number" parameter is deprecated for "octokit.issues.createComment()". Use "issue_number" instead'
-  //         );
-  //       }
-  //     }
-  //   });
-  //   // See https://developer.github.com/v3/issues/comments/#create-a-comment
-  //   const { data } = await octokit.issues.createComment({
-  //     owner: "octocat",
-  //     repo: "hello-world",
-  //     number: 123,
-  //     issue_number: 456,
-  //     body: "Hello there!"
-  //   });
-  //   expect(data).toStrictEqual({ ok: true });
-  //   expect(warnCalledCount).toEqual(1);
-  // });
+  it("deprecated parameter", async () => {
+    const mock = fetchMock
+      .sandbox()
+      .putOnce("path:/repos/octocat/hello-world/actions/secrets/MY_SECRET", {
+        ok: true,
+      });
+    const MyOctokit = Octokit.plugin(restEndpointMethods);
+
+    let warnCalledCount = 0;
+    const octokit = new MyOctokit({
+      request: {
+        fetch: mock,
+      },
+      log: {
+        warn: (deprecation: Error) => {
+          warnCalledCount++;
+          expect(deprecation).toMatch(
+            '"name" parameter is deprecated for "octokit.actions.createOrUpdateRepoSecret()". Use "secret_name" instead'
+          );
+        },
+      },
+    });
+    // See https://developer.github.com/v3/actions/secrets/#create-or-update-a-repository-secret
+    // The `:secret_name` URL parameter was `:name` until May 14, 2020
+    // @ts-ignore
+    const { data } = await octokit.actions.createOrUpdateRepoSecret({
+      owner: "octocat",
+      repo: "hello-world",
+      name: "MY_SECRET",
+    });
+    expect(data).toStrictEqual({ ok: true });
+    expect(warnCalledCount).toEqual(1);
+  });
+
+  it("deprecated parameter + new parameter", async () => {
+    const mock = fetchMock
+      .sandbox()
+      .putOnce("path:/repos/octocat/hello-world/actions/secrets/MY_SECRET2", {
+        ok: true,
+      });
+    const MyOctokit = Octokit.plugin(restEndpointMethods);
+
+    let warnCalledCount = 0;
+    const octokit = new MyOctokit({
+      request: {
+        fetch: mock,
+      },
+      log: {
+        warn: (deprecation: Error) => {
+          warnCalledCount++;
+          expect(deprecation).toMatch(
+            '"name" parameter is deprecated for "octokit.actions.createOrUpdateRepoSecret()". Use "secret_name" instead'
+          );
+        },
+      },
+    });
+    // See https://developer.github.com/v3/actions/secrets/#create-or-update-a-repository-secret
+    // The `:secret_name` URL parameter was `:name` until May 14, 2020
+    // @ts-ignore
+    const { data } = await octokit.actions.createOrUpdateRepoSecret({
+      owner: "octocat",
+      repo: "hello-world",
+      name: "MY_SECRET1",
+      secret_name: "MY_SECRET2",
+    });
+    expect(data).toStrictEqual({ ok: true });
+    expect(warnCalledCount).toEqual(1);
+  });
+
+  it("deprecated method + deprecated parameter", async () => {
+    const mock = fetchMock
+      .sandbox()
+      .putOnce("path:/repos/octocat/hello-world/actions/secrets/MY_SECRET", {
+        ok: true,
+      });
+    const MyOctokit = Octokit.plugin(restEndpointMethods);
+
+    const deprecations: string[] = [];
+    const octokit = new MyOctokit({
+      request: {
+        fetch: mock,
+      },
+      log: {
+        warn: (deprecation: Error) => {
+          deprecations.push(deprecation.toString());
+        },
+      },
+    });
+    // See https://developer.github.com/v3/actions/secrets/#create-or-update-a-repository-secret
+    // The `:secret_name` URL parameter was `:name` until May 14, 2020
+    // @ts-ignore
+    const { data } = await octokit.actions.createOrUpdateSecretForRepo({
+      owner: "octocat",
+      repo: "hello-world",
+      name: "MY_SECRET",
+    });
+    expect(data).toStrictEqual({ ok: true });
+    expect(deprecations.length).toEqual(2);
+    expect(deprecations.sort()).toStrictEqual([
+      '"name" parameter is deprecated for "octokit.actions.createOrUpdateSecretForRepo()". Use "secret_name" instead',
+      "octokit.actions.createOrUpdateSecretForRepo() has been renamed to octokit.actions.createOrUpdateRepoSecret()",
+    ]);
+  });
 });
