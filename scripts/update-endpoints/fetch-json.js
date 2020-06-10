5f4477d58c133d48f7018bc513670d7365171975
Gregor Martynus <gregor@martynus.net>
2020-06-10 11:43:56 -0700

    build(routes-update): ignore legacy endpoints

--- scripts/update-endpoints/fetch-json.js
+++ scripts/update-endpoints/fetch-json.js
@@ -10,7 +10,7 @@ if (!process.env.VERSION) {
 
 const QUERY = `
   query ($version: String!, $ignoreChangesBefore: String!) {
-    endpoints(version: $version, ignoreChangesBefore: $ignoreChangesBefore, filter: { isGithubCloudOnly: false }) {
+    endpoints(version: $version, ignoreChangesBefore: $ignoreChangesBefore, filter: { isLegacy: false, isGithubCloudOnly: false }) {
       name
       scope(format: CAMELCASE)
       id(format: CAMELCASE)
