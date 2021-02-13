a54118221a30621b2e92ce3a560caea9ade4fcd8
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-02-12 22:14:04 -0800

    refactor: remove obsolete comment and `deprecation` import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Octokit } from "@octokit/core";
-import { Deprecation } from "deprecation";
 
 import ENDPOINTS from "./generated/endpoints";
 export { RestEndpointMethodTypes } from "./generated/parameters-and-response-types";
@@ -7,16 +6,6 @@ import { VERSION } from "./version";
 import { Api } from "./types";
 import { endpointsToMethods } from "./endpoints-to-methods";
 
-/**
- * This plugin is a 1:1 copy of internal @octokit/rest plugins. The primary
- * goal is to rebuild @octokit/rest on top of @octokit/core. Once that is
- * done, we will remove the registerEndpoints methods and return the methods
- * directly as with the other plugins. At that point we will also remove the
- * legacy workarounds and deprecations.
- *
- * See the plan at
- * https://github.com/octokit/plugin-rest-endpoint-methods.js/pull/1
- */
 export function restEndpointMethods(octokit: Octokit): Api {
   return endpointsToMethods(octokit, ENDPOINTS);
 }
