132f2714807f20a7e7321b6aaa44e64e406cf062
wolfy1339 <webmaster@wolfy1339.com>
2024-04-30 13:55:15 -0400

    fix(types): export `Api`
    This resolves issues in consumers of this package getting errors from TypeScript
    
    ```
    The inferred type of 'Octokit' cannot be named without a reference to '../node_modules/@octokit/plugin-rest-endpoint-methods/dist-types/types.js'. This is likely not portable. A type annotation is necessary.
    ```

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { VERSION } from "./version.js";
 import type { Api } from "./types.js";
 import { endpointsToMethods } from "./endpoints-to-methods.js";
 
+// Export the type for downstream users in order to fix a TypeScript error
+// The inferred type of 'Octokit' cannot be named without a reference to '../node_modules/@octokit/plugin-rest-endpoint-methods/dist-types/types.js'. This is likely not portable. A type annotation is necessary.
+export type { Api };
+
 export function restEndpointMethods(octokit: Octokit): Api {
   const api = endpointsToMethods(octokit);
   return {
